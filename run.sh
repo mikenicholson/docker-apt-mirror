af608ddcda4926dbaeb4f5daa63049301ede5c9d
Mike Nicholson <mike.nicholson@viavisolutions.com>
2016-04-12 09:49:03 -0400

    Start the webserver first
    This won't be good for first time syncs but makes starting a new
    container much more pleasant when you're mounting an already populated
    mirror volume

--- run.sh
+++ run.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
+# Start nginx to begin serving the mirror
+echo "Starting webserver"
+nginx -g 'daemon off;' &
+
 # Run once to perform initial sync
 echo "Performing intial sync"
 /usr/bin/apt-mirror
 
 # Start cron to force nightly syncs
+# Don't start cron before the first call to apt-mirror
+# finishes since it can take a very long time.  I'm assuming
+# running multiple apt-mirror's would not be good
 echo "Starting cron for subsequent syncs"
 cron
 
-# Start nginx to begin serving the mirror
-echo "Starting webserver"
-nginx
 
-multitail /var/log/nginx/trusty-mirror.access.log /var/log/nginx/trusty-mirror.error.log 
+# Wait for the webserver... forever
+wait
